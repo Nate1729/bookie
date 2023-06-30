a10b1e037406c626db95ae3749f2948ba847b5e0
Nate <nferreira1729@gmail.com>
2023-06-30 18:11:24 -0400

    tests: add tests for main file (#28)
    * refactor: move code into main function
    
    * refactor: refactor main module to make testing easier
    
    * tests: add tests for main module
    
    * ci: add coverage config file
    
    ignore main fn call
    update gitignore to vc coverage config file
    
    * style: run black

--- bookie/main.py
+++ bookie/main.py
@@ -6,20 +6,26 @@ from typing import Optional
 from bookie import summary, arg_parser, models
 
 
-def process_file(file_path: str) -> Optional[list[models.Receipt]]:
+def process_file(file_path: str) -> list[models.Receipt]:
     with open(file_path, "r") as f:
         entries = [models.receipt_from_dict(line) for line in csv.DictReader(f)]
         return entries
 
 
-if __name__ == "__main__":
+def _get_cli_args() -> list[str]:
+    return sys.argv
+
+
+def main():
     try:
-        file_name = arg_parser.validate_arguments(sys.argv)
+        file_name = arg_parser.validate_arguments(_get_cli_args())
     except arg_parser.InputError as e:
         print(f'There was an issue with the inputs.\nError: "{e}"')
         sys.exit(1)
 
     data = process_file(file_name)
-    if data is None:
-        sys.exit(1)
     summary.summarize(data)
+
+
+if __name__ == "__main__":
+    main()
